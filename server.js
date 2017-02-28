f53b0f36e728e109790359863ebceba7a6ca0145
wave-inguane <avelinoinguane@hotmail.com>
2017-02-28 17:00:19 -0500

    clean up

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ var app = express();
 var bodyParser = require('body-parser');
 var Firebase = require("firebase");
 var firebaseStudyURL = 'https://programmingstudies.firebaseio.com/studies/microtaskWorkflow/test1';
-var pastebinURL = 'https://seecoderun.firebaseapp.com/#-';
+var pastebinURL = 'https://seecoderun.firebaseapp.com/#:-';//'https://seecode.run/#:-';//
 var nextSession;             // JSON structure for the next session
+var screenTaskTime;          //time spent on screening task
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client'));
 app.set('port',(process.env.PORT || 5001));
-app.set('views', './views');
-app.set('view engine', 'mustache');
+//app.set('views', './views');
+//app.set('view engine', 'mustache');
 
 //Admin action to initialize a new study by creating a set of workflows.
 app.get('/createWorkflows', function (req, res) {
@@ -37,46 +38,27 @@ app.get('/', function(req, res){
 // When a user first hits the study server, first check if they have already participated.
 // If not, send them to the screening test.
 //.........................................................................................
-app.get('/screeningTask', function (req, res) {
-    //var workerId = req.query.workerId;
-    var workerId = req.body.workerID;
 
-    //TODO: require workerID before screening ?
-    console.log("WORKER ID: " + workerId);
+app.post('/screeningTask', function (req, res) {
 
-    // Check if there is already data for this worker in Firebase.
-    // If there is, the worker has already participated.
-    var workerRef = new Firebase(firebaseStudyURL + '/workers/' + workerId);
-    workerRef.once('value', function(snapshot) {
-        if (snapshot.val() == null)
-        {
-            // TODO: We need to be able to send back to the client the id of the worker.
-            // Options: (1) we could use a templating engine to embed it in the code.
-            // Probably need to set up one of the mustache processors.
-            // We could also then use templating to set up worker specific content in the templates.
-            // Like customizing the instructions. Or hiding or showing the chat system for different versions.
-
-            res.sendFile(__dirname + '/client/screening.html');///?workerId=5'); // ?workerId=' + workerId);
-        }
-        else
-        {
-            res.sendFile(__dirname + '/client/alreadyParticipated.html');
-        }
-    });
+    res.sendFile(__dirname + '/client/screening.html');
+    // res.sendFile(__dirname + '/client/alreadyParticipated.html');
 });
+
 
 //.................................................................................................
 // After finishing the screening, check if they passed. If so, send them to the demographics page.
 //.................................................................................................
-app.post('/screenSubmit', function (req, res) {
-    var taskTime = req.body.taskTimeMillis;
+app.post('/screenSubmitted', function (req, res) {
+    screenTaskTime = req.body.taskTimeMillis;
     var result = req.body.question1;
 
     console.log('screening submitted'); 
-    console.log(result + " " + taskTime);
+    console.log(result + " " + screenTaskTime);
 
-    if((result == 7) && (taskTime <= 600000))
+    if(result == 7) {
         res.sendFile(__dirname + '/client/demographics.html');
+    }
     else
         res.sendFile(__dirname + '/client/failedScreening.html');
 });
@@ -84,12 +66,13 @@ app.post('/screenSubmit', function (req, res) {
 //..................................................................................................
 // After finishing the demographics survey, send the user to the waiting room.
 //..................................................................................................
-app.post('/waitingroom', function (req, res) {
+app.post('/demographics', function (req, res) {
      // TODO: store the demographics data to firebase, associated with the participant.
+    //DONE
      var workerId = req.body.workerID;
-     var session = req.body.session.valueOf();
      var currstatus = req.body.currentstatus;
-		 var yearsOfExp = req.body.yearsExp;
+     var yearsOfProgramExp = req.body.yearsExp; //just programming experience
+     var yearsOfDevExp = req.body.workDevExp; //professional experience
 
     // Check if there is already data for this worker in Firebase.
     // If there is, the worker has already participated.
@@ -97,40 +80,49 @@ app.post('/waitingroom', function (req, res) {
     workerRef.once('value', function(snapshot) {
         if (snapshot.val() == null) {
             //add new worker
-            workerRef.push({ 'workerId': workerId,
-                             'session': session,
-														 'status':currstatus,
-														 'experience':yearsOfExp});
+            workerRef.push({
+                'workerId': workerId,
+                'status': currstatus,
+                'languageExp': yearsOfProgramExp,
+                'developerExp': yearsOfDevExp,
+                'screenTime': screenTaskTime + " ms"
+            });
+            //reset
+            screenTaskTime = null;
 
-            console.log('WORKER ID: ' + workerId );
-            if(session == "single"){
-                console.log('DO TASK');
-                res.sendFile(__dirname + '/client/index_content.html');
-            }else
-            res.sendFile(__dirname + '/client/waitingRoom.html');
+            console.log('WORKER ID: ' + workerId);
+
         }
     });
 
 });
 
 
-
 // Start the server.
 var server = app.listen(app.get('port'), function () {
     var host = server.address().address;
     var port = server.address().port;
-    firebaseSetup();
+
+    if (nextSession == null)
+        createWorkflows();
+
+        firebaseSetup();
 
     console.log('http://localhost:' + port + '/');
 });
 
+//............................................................................................
+// Create wait list
+//............................................................................................
 function firebaseSetup()
 {
+    console.log('In func firebaseSetup()');
     var waitListRef = new Firebase(firebaseStudyURL + '/waitlist');
 
     // Watch for additions to the waitingList. Once the waiting list meets or exceeds the size of the
     // number of participants required for the nextSession, start the session.
     waitListRef.on("value", function(snapshot) {
+
         if (nextSession != null)
         {
             var waitlistSizeEstimate = snapshot.numChildren();
@@ -151,11 +143,13 @@ function firebaseSetup()
                     startSession(nextSession, snapshot);
                 }
             }
-        }
+        } //end if
     });
 }
 
+//..............................................................................................
 // Create the workflows on Firebase and the corresponding initial sessions for each workflow.
+//..............................................................................................
 function createWorkflows()
 {
     var totalWorkflowCount = 2;
@@ -168,7 +162,7 @@ function createWorkflows()
         var workflow = {};
         workflow.workflowURL = pastebinURL + 'workflowAAAA' + i;
         workflow.timeLimitMins = 10;
-        workflow.participantsPerSession = 1;
+        workflow.participantsPerSession = 2;
         workflow.totalSessions = 1;
         var workflowID = i;
         workflows[workflowID] = workflow;
@@ -199,23 +193,30 @@ function createWorkflows()
     nextSession = sessions[0];
 }
 
+//.......................................................................
 // Starts the corresponding session.
+//.......................................................................
 function startSession(session, waitlistSnapshot)
 {
     // Load status
+    var nextSessionId;
     var statusRef = new Firebase(firebaseStudyURL + '/status');
     statusRef.once("value", function(snapshot)
     {
         if (snapshot.val() != null)
         {
             var status = snapshot.val();
+            nextSessionId = status.nextSessionID;
 
             // Build the list of IDs for the workers who will be working on this session.
             var workers = {};
             var i = 0;
             waitlistSnapshot.forEach(function(waitlistEntrySnapshot) {
-                workers[i] = waitlistEntrySnapshot.val().userID;
+                workers[i] = waitlistEntrySnapshot.val().workerId;
                 i++;
+
+                console.log("SESSION "+ nextSessionId);
+
                 // If we've selected all of the participants, break.
                 if (i >= session.totalParticipants)
                     return true;    // break
@@ -226,7 +227,9 @@ function startSession(session, waitlistSnapshot)
             session.startTime = date.toDateString() + ' '  + date.toTimeString();
             session.startTimeMillis = date.getTime();
             session.workers = workers;
-            var sessionRef = new Firebase(firebaseStudyURL + '/sessions/' + session.sessionID);
+            session.sessionID = nextSessionId;
+           var sessionRef = new Firebase(firebaseStudyURL + '/sessions/' + session.sessionID);
+           // var sessionRef = new Firebase(firebaseStudyURL + '/sessions/' + nextSessionId);
             sessionRef.set(session);
 
             // Increment the next session to be started.
@@ -242,8 +245,8 @@ function startSession(session, waitlistSnapshot)
             // We do this last to ensure that the session is now fully set up.
             i = 0;
             waitlistSnapshot.forEach(function(waitlistEntrySnapshot) {
-                var workerWaitlistRef = new Firebase(firebaseStudyURL + '/waitlist/' + waitlistEntrySnapshot.key() +
-                    '/sessionURL');
+                var workerWaitlistRef = new Firebase(firebaseStudyURL + '/waitlist/'
+                    + waitlistEntrySnapshot.key() + '/sessionURL');
                 workerWaitlistRef.set(session.workflowURL);
 
                 i++;
@@ -253,7 +256,7 @@ function startSession(session, waitlistSnapshot)
             });
              
 
-            // TODO: Set a timeout to be able to end the session when the time is up???
+            // TODO: Set a timeout to be able to end the session when the time is up
             //set a timer, end it even if submit is not clicked
             //onDisconnect() on Fire. timer on client side
         }
