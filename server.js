084811fb722b9cda4ed44a6ac37eb41475e4ae7c
wave-inguane <avelinoinguane@hotmail.com>
2017-04-03 16:41:29 -0400

    session update complete

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ var server = app.listen(app.get('port'), function () {
 //..............................................................................................
 function createWorkflows()
 {
-    var totalWorkflowCount = 2;
+    var totalWorkflowCount = 4;
 
     var workflows = {};
     //var sessions = {};//moved to global field area
